c51c11f1550dfa9d8ff4b111db69fe7fcd643784
Olga Vishnievska <olgacherry03@gmail.com>
2023-05-09 23:03:20 +0300

    Added some authorization pages

--- language_course/urls.py
+++ language_course/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 
 urlpatterns = [
+    path('login/', TemplateView.as_view(template_name="login.html")),
+    path('register/', TemplateView.as_view(template_name="register.html")),
+    path('reset-password/', TemplateView.as_view(template_name="reset-password.html")),
+    path('confirmation/', TemplateView.as_view(template_name="confirmation.html")),
     path('admin/', admin.site.urls),
     path('api/', include('course.urls')),
     path('api/', include('djoser.urls')),
